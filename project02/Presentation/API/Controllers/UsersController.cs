ab698a9936c7aab87c1d9d510fd12b61400b8794
jonaferreir4 <jonaferreira2204@gmail.com>
2025-03-05 16:51:03 -0300

    feat: IV - Configuration persistence and Validator using FluentValidation

--- project02/Presentation/API/Controllers/UsersController.cs
+++ project02/Presentation/API/Controllers/UsersController.cs
@@ -23,13 +23,13 @@ namespace API.Controllers
         [HttpPost]
         public async Task<ActionResult<CreateUserResponse>> Create (CreateUserRequest request, CancellationToken cancellationToken) {
             
-            var validator = new CreateUserValidator();
-            var validatorResult = await validator.ValidateAsync(request);
+            // var validator = new CreateUserValidator();
+            // var validatorResult = await validator.ValidateAsync(request);
 
-            if(!validatorResult.IsValid){
+            // if(!validatorResult.IsValid){
 
-                return BadRequest(validatorResult.Errors);
-            }
+            //     return BadRequest(validatorResult.Errors);
+            // }
             
             var response =  await _mediator.Send(request, cancellationToken);
             return Ok(response);
--- project02/Presentation/API/Program.cs
+++ project02/Presentation/API/Program.cs
@@ -3,10 +3,9 @@ using Repositories;
 using Microsoft.Extensions.DependencyInjection;
 using Persistence.Context;
 
-
 var builder = WebApplication.CreateBuilder(args);
 
-
+// Configuração dos serviços
 builder.Services.ConfigurePersistenceApp(builder.Configuration);
 builder.Services.ConfigureApplicationApp();
 
@@ -16,26 +15,26 @@ builder.Services.AddSwaggerGen();
 
 var app = builder.Build();
 
-
+// Método para garantir que o banco de dados seja criado
 static void CreateDatabase(WebApplication app)
 {
-    using (var scope = app.Services.CreateScope()) 
+    using (var scope = app.Services.CreateScope())
     {
         var services = scope.ServiceProvider;
         try
         {
-            var context = services.GetRequiredService<AppDbContext>(); 
-            context.Database.EnsureCreated();        
-            
-            }
+            var context = services.GetRequiredService<AppDbContext>();
+            context.Database.EnsureCreated();
+        }
         catch (Exception ex)
         {
-            var logger = services.GetRequiredService<ILogger<Program>>(); 
+            var logger = services.GetRequiredService<ILogger<Program>>();
             logger.LogError(ex, "Ocorreu um erro ao criar o banco de dados.");
         }
     }
 }
 
+// Chamar a criação do banco de dados ANTES de inicializar o restante da aplicação
 CreateDatabase(app);
 
 if (app.Environment.IsDevelopment())
@@ -43,5 +42,6 @@ if (app.Environment.IsDevelopment())
     app.UseSwagger();
     app.UseSwaggerUI();
 }
+
 app.MapControllers();
 app.Run();
